df57a46f3070dfc0007b41f1dd4492828dc45b8f
Epadilla1886 <ejpadilla1886@gmail.com>
2019-04-19 12:19:49 -0700

    added multiple comprehensive and advance functions

--- calculator.js
+++ calculator.js
@@ -1,76 +1,147 @@
-var numberArray = [];
+$(document).ready(initializeApp);
 
-
-$(document).ready(function(){
-    clickEvents();
-    displayValue();
-
-});
-    function clickEvents() {
-        $('.btn_number').click(btnPressed);
-        $('.btn_operator').click(handleOperator);
-        $('.btn_math').click(do_math);
-        $('.btn_decimal').click(btnPressed);
-        $('.btn_clear').click(clearArray);
-        $('.btn_clear_display').click(displayOpClear);
-    }
-
-    function btnPressed(button) {
-        var btnValue = $(this).val();
-        console.log(btnValue);    //DELETE
-        if (numberArray.length === 0) {
-            numberArray.push(btnValue);
-        } else if (Number(numberArray[numberArray.length - 1])) {
-            numberArray[numberArray.length - 1] += btnValue;
-        } else if (numberArray.length > 1 && !Number(numberArray[numberArray.length - 1])){
-            numberArray.push(btnValue);}
-        displayOpClear();
-    }
-
-    function handleOperator(button) {
-        var opBtnVal = $(this).val();
-        var newOpBtnVal = $(this).val();
-        console.log(opBtnVal);
-        if(!Number(numberArray[numberArray.length-1])){
-            numberArray.pop();
-            numberArray.push(opBtnVal);
-        } else if (numberArray.length >= 0) {
-            numberArray.push(opBtnVal);
-        }
-        displayOpClear();
-    }
-
-function do_math() {
-    var num1 = Number(numberArray[0]);
-    var num2 = Number(numberArray[2]);
-    var operator = numberArray[1];
-
-    if(operator === '+'){
-         var result = num1+num2;
-    } else if ( operator === '-'){
-        result = num1-num2;
-    } else if ( operator === '*'){
-        result = num1*num2;
-    } else if ( operator === '/'){
-        result =  num1/num2;
-    }
-    displayResult(result);
-    numberArray = [];
+function initializeApp() {
+	clickHandler();
 }
 
-function clearArray() {
-     numberArray = [];
-     displayValue();
-    }
+var doMathArray = [];
+var decimalToggle = false;
 
-function displayValue() {
-    $('.display').text(numberArray);
-    }
+function clickHandler() {
+	$(".display").text("0");
+	$(".button.number").click(addNumberToEquation);
+	$(".button.clear").click(clearMathArray);
+	$(".button.clear-e").click(clearMathArray);
+	$(".button.operator").click(addOperatorToEquation);
+	$(".button.equals").click(equalCheck);
+	$(".button.decimal").click(addDecimalToEquation);
+	$(".button.negative").click(makeNegative);
+}
 
-function displayOpClear(){
+function addNumberToEquation() {
+	var numberClicked = $(this).text();
+	if (doMathArray.length === 0) {
+		doMathArray.push(numberClicked);
+		$(".display").text(doMathArray[doMathArray.length-1]);
+	} else if (doMathArray[doMathArray.length-1] === "0.") {
+		doMathArray[doMathArray.length-1] += numberClicked;
+		$(".display").text(doMathArray[doMathArray.length-1]);
+	} else if (Number(doMathArray[doMathArray.length-1])){
+		doMathArray[doMathArray.length-1] += numberClicked;
+		$(".display").text(doMathArray[doMathArray.length-1]);
+	} else if (!Number(doMathArray[doMathArray.length-1])){
+		doMathArray.push(numberClicked);
+		$(".display").text(doMathArray[doMathArray.length-1]);
+		$(".clicked").removeClass("clicked");
+	}
+}
 
-    $('.display').text(numberArray[numberArray.length-1])
-    }
-function displayResult(result) {
-    $('.display').text(result);
+function addDecimalToEquation() {
+	if (decimalToggle === true) {
+		return;
+	}
+	var decimalClicked = $(this).text();
+	$(".clicked").removeClass("clicked");
+	if (doMathArray.length === 0) {
+		doMathArray.push("0" + decimalClicked);
+		$(".display").text(doMathArray[doMathArray.length-1]);
+	} else if (Number(doMathArray[doMathArray.length-1])){
+		doMathArray[doMathArray.length-1] += decimalClicked;
+		$(".display").text(doMathArray[doMathArray.length-1]);
+	} else if (doMathArray[doMathArray.length-1] === "+" || doMathArray[doMathArray.length-1] === "-" || doMathArray[doMathArray.length-1] === "*" || doMathArray[doMathArray.length-1] === "/") {
+		doMathArray.push("0" + decimalClicked);
+		$(".display").text(doMathArray[doMathArray.length-1]);
+	}
+	decimalToggle = true;
+}
+
+function addOperatorToEquation() {
+	var operatorClicked = $(this).text();
+	if (doMathArray.length === 0){
+		$(".display").text(operatorClicked);
+	} else if (Number(doMathArray[doMathArray.length-1])){
+		doMathArray.push(operatorClicked);
+		$(".display").text(doMathArray[doMathArray.length-1]);
+		$(this).addClass("clicked");
+	} else if (!Number(doMathArray[doMathArray.length-1]) && doMathArray[doMathArray.length-1] !== operatorClicked){
+		$(".clicked").removeClass("clicked");
+		doMathArray.pop(doMathArray[doMathArray.length-1]);
+		doMathArray.push(operatorClicked);
+		if ( operatorClicked === "*" ) {
+			$(".display").text("x");
+		} else if ( operatorClicked === "/" ) {
+			$(".display").text("÷");
+		} else {
+			$(".display").text(doMathArray[doMathArray.length-1]);
+		}
+		$(this).addClass("clicked");
+	}
+	decimalToggle = false;
+}
+
+function equalCheck () {
+	displayValue = $(".display").text();
+	if (doMathArray.length === 0){
+		$(".display").text("Ready");
+	} else if (doMathArray.length-1 === 0) {
+		var result = doMathArray[doMathArray.length-1];
+		$(".display").text(result);
+	} else if (doMathArray.length-1 === 1){
+		doMathArray.push(doMathArray[doMathArray.length-2]);
+		doMath(doMathArray);
+	} else if (doMathArray[doMathArray.length-1] === "0" && doMathArray[doMathArray.length-2] === "/"){
+		$(".display").text("Error");
+	} else if (doMathArray.length > 3) {
+		var splicedArray = doMathArray.splice(0,3);
+		result = doMath(splicedArray);
+		resultToString = result.toString();
+		doMathArray.splice(0, 0, resultToString);
+		equalCheck();
+	} else if (doMath(doMathArray).toString() === displayValue) {
+		if (doMathArray.length === 3) {
+			doMathArray.splice(0, 1, displayValue);
+			doMath(doMathArray);
+		} else if (doMathArray.length > 3) {
+			doMathArray.splice(0, doMathArray[doMathArray.length-2], displayValue);
+			doMath(doMathArray);
+		}
+	} else {
+		doMath(doMathArray);
+	}
+
+}
+
+function doMath ( array ) {
+	if (array[1] === "+") {
+		var result = Number(array[0]) + Number(array[2]);
+	} else if (array[1] === "-") {
+		result = array[0] - array[2];
+	} else if (array[1] === "*") {
+		result = array[0] * array[2];
+	} else if (array[1] === "/") {
+		result = array[0] / array[2];
+	}
+	$(".display").text(result);
+	decimalToggle = false;
+	return result;
+}
+
+function clearMathArray () {
+	var clearButtonClicked = $(this).text();
+	if (clearButtonClicked === "C"){
+		doMathArray = [];
+		$(".display").text("0");
+	} else if (clearButtonClicked === "CE"){
+		doMathArray.pop();
+		$(".display").text("0");
+	}
+	$(".clicked").removeClass("clicked");
+	decimalToggle = false;
+}
+
+function makeNegative (){
+	var lastNum = doMathArray.pop();
+	var negNumber = lastNum*-1;
+	doMathArray.push(negNumber);
+    $(".display").text(negNumber)
 }
